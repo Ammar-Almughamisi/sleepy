c15b85a50fb5c4bc665ac736d414c1ed9f7eb6c9
ammar <ammar.almughamisi@gmail.com>
2019-08-16 03:36:00 +0300

    mew nbramcj

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,8 @@ package com.company;
 
 public class Main {
     public void add(int a, int b) {
-        System.out.println(a + b);
-        System.out.println("wtf");
+
+
     }
 
     public static void main(String[] args) {
