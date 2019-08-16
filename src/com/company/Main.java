762a41f9b218cd06247ac42956b72328b192dabc
ammar <ammar.almughamisi@gmail.com>
2019-08-16 03:29:03 +0300

    totot

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
 public class Main {
+    public void add(int a, int b) {
+        System.out.println(a + b);
+    }
 
     public static void main(String[] args) {
-	// write your code here
     }
 }
