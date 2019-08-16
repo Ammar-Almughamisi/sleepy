f85a569c3d01719747d086a41f9d7bdce5230624
ammar <ammar.almughamisi@gmail.com>
2019-08-16 03:34:16 +0300

    kaap6

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
     public void add(int a, int b) {
         System.out.println(a + b);
+        System.out.println("wtf");
     }
 
     public static void main(String[] args) {
